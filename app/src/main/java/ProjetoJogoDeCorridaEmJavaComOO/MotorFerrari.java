97f6470d0d264d470c8a28c28de537a9b76e3ee5
Lophezz <lopes.oliveira334@gmail.com>
2022-08-05 21:29:32 -0300

    Inicializei o basicao do jogo de corrida que sao instancias de carros que recebem diferentes tipos de motor

---
+++ app/src/main/java/ProjetoJogoDeCorridaEmJavaComOO/MotorFerrari.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjetoJogoDeCorridaEmJavaComOO;
+
+/**
+ *
+ * @author Admin
+ */
+public class MotorFerrari implements iMotor{
+
+    @Override
+    public String acelerar() {
+    return "VRUMMMMMMMM!!!!(Ferrari)";
+    }
+}
